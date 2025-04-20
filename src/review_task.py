ad5c7265c00dbabf35b3458406c41dd63dde34a5
Sinethemba-Godlo-25 <godlosinethemba@gmail.com>
2025-04-20 10:27:11 +0200

    Update review_task.py

--- src/review_task.py
+++ src/review_task.py
@@ -1,12 +1,12 @@
 class ReviewTask:
-    def __init__(self, task_id, reviewer, access_request):
-        self.__task_id = task_id
-        self.__reviewer = reviewer  # Association to User
-        self.access_request = access_request # Association to AccessRequest
- 
+    def __init__(self, task_id, access_request, reviewer):
+        self.task_id = task_id
+        self.access_request = access_request
+        self.reviewer = reviewer
+        self.is_approved = None  # Assume the task starts as not approved
 
-    def review(self, approve=True):
-        if approve:
-            self.access_request.approve()
-        else:
-            self.access_request.reject()
+    def approve(self):
+        self.is_approved = True
+
+    def reject(self):
+        self.is_approved = False
